3b2b3d472abbcc4fb669c42954fb42e96ade5ef1
ninantharakan <ninan.tharakan@gmail.com>
2017-09-10 11:30:24 -0400

    Update Master_contract.sol

--- contracts/Master_contract.sol
+++ contracts/Master_contract.sol
@@ -7,12 +7,29 @@ contract Master_Contract {
   mapping (address => string) userIDs;
   mapping (string => string) roles;
   mapping (string => address) userAddresses;
+  address adminAddr;
+
+  function Master_Contract(){
+  adminAddr=msg.sender;
+  }
+  
  
-  function addUser(string _userID, string _pwd, string _role) returns (bool) {
-  User_Contract con = new User_Contract(_userID, _pwd, msg.sender);
+  function addUser(string _userID, string _pwd,string _role) returns (bool) {
+  if((sha3(_role)==sha3("Certifier")) && (msg.sender!=adminAddr)) // strings can't be compared directly hence using sha3()
+  {return false;}
+  User_Contract con = new User_Contract(_userID, _pwd,_role,msg.sender);
   userAddresses[_userID] = address(con);
   userIDs[con] = _userID;
   roles[_userID] = _role;
+  return true;
+  }
+  
+  function deleteUser(string userId) returns (bool){
+  if(userAddresses[userId]!=msg.sender || adminAddr!=msg.sender)
+  {return false;}
+  address uadd = userAddresses[userId];
+  delete userAddresses[userId];
+  delete userIDs[uadd];
   return true;
   }
   
@@ -23,12 +40,8 @@ contract Master_Contract {
   function getAddress(string userid) returns (address){
   return userAddresses[userid];
   }
-  
-  function deleteUser(string userId) returns (bool){
-  address uadd = userAddresses[userId];
-  delete userAddresses[userId];
-  delete userIDs[uadd];
-  return true;
+  function getuserID(address useraddress) returns (string){
+  return userIDs[useraddress];
   }
   
 }
